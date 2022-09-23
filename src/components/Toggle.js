97faccfbbda9ca2ba7b35fd20d237297d2a87a13
Daniel Macharia <danielhuios@gmail.com>
2022-09-23 13:41:59 +0300

    beautiful on-off toggle button

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+	const [isOn, setIsOn] = useState(true);
+
+	function handleClick() {
+		setIsOn((isOn) => !isOn);
+	}
+
+	const color = isOn ? "green" : "red";
+
+	return (
+		<button style={{ backgroundColor: color }} onClick={handleClick}>
+			{" "}
+			{isOn ? "ON" : "OFF"}
+		</button>
+	);
 }
 
 export default Toggle;
